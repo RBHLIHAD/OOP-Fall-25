f46c86fba33363009f77475762257925a3239e17
MD. RIFAT-BIN-HOSSAIN <rbhlihad@gmail.com>
2025-09-10 19:51:11 +0600

    Add files via upload

---
+++ EmployeeDetails.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.employeedetails;
+
+public class EmployeeDetails {
+
+    public static void main(String[] args) {
+        
+        String name="Rifat";
+        int id=1157;
+        double salary=2000;
+        double annualsalary=salary*12;
+        
+        System.out.println("name= "+name);
+        System.out.println("id= "+id);
+        System.out.println("salary= "+salary);
+        System.out.println("annualsalary= "+annualsalary);
+    }
+}
