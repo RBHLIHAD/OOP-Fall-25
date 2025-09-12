b9c787600311f9558d799f5971e367986c08075b
MD. RIFAT-BIN-HOSSAIN <rbhlihad@gmail.com>
2025-09-12 11:51:47 +0600

    Add files via upload

---
+++ DeterminesANumber.java
@@ -0,0 +1,24 @@
+
+package com.mycompany.determinesanumber;
+
+
+public class DeterminesANumber {
+
+public static void main(String[] args) {
+
+        double a = 15;
+        double b = 10;
+        double c = 30;
+
+        if (a < b && a < c) {
+            System.out.println("a is the smallest number");
+        } 
+        else if (b < a && b < c) {
+            System.out.println("b is the smallest number");
+        } 
+        else {
+            System.out.println("c is the smallest number");
+        }
+
+    }
+}
