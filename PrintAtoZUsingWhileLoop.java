b5b8334e8090d93dbd67f5a70e7e0e21a66ad5ce
MD. RIFAT-BIN-HOSSAIN <rbhlihad@gmail.com>
2025-09-12 12:58:05 +0600

    Update PrintAtoZUsingWhileLoop.java

--- PrintAtoZUsingWhileLoop.java
+++ PrintAtoZUsingWhileLoop.java
@@ -12,7 +12,7 @@ public class PrintAtoZUsingWhileLoop {
         {
             System.out.println(" " +i);
             
-            if(i=='T')
+            if(i=='k')
             {
                 break;
             }
